af6abed7e3250799ebce7e3831214d7d4c317a2c
E3ND <brunopimentelcityhotmail.com@gmail.com>
2025-03-11 13:02:21 -0300

    📝 (Program.java): add support for promoting a pawn to a different piece (Bishop, Knight, Queen, Rook) when it reaches the opposite end of the board 📝 (ChessMatch.java): add logic to handle pawn promotion and replace it with the chosen piece (Bishop, Knight, Queen, Rook)

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,18 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q)");
+					String type = sc.nextLine().toUpperCase();
+					
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.println("Enter piece for promotion (B/N/R/Q)");
+						type = sc.nextLine().toUpperCase();
+					}
+					
+					chessMatch.replacePromotedPiece(type);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
